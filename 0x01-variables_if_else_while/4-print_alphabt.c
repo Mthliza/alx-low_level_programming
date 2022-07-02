815d0b7df94f7fff55b4cf7dba437c1a674dd0ea
Mthliza <thlizamoses@gmail.com>
2022-07-02 05:27:22 -0700

    variables

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -13,11 +13,11 @@ int main(void)
 char ch;
 for (ch = 'a'; ch <= 'z'; ch++)
 {
-if (ch != 'e' && ch != 'q')
+if (ch != 'q' && ch != 'q')
 putchar(ch);
 }
-putchar(10);
-}
+putchar('\n');
+
 return (0);
 }
 
