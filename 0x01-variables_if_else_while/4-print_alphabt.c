0438be9fb378b4b2f6b0473e02d92c9d5f3d7212
Mthliza <thlizamoses@gmail.com>
2022-07-02 05:34:37 -0700

    variables if

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,23 +1,22 @@
-#include <stdio.h>
+#include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
 
 /**
- *main - Prints some letters of alphabet
- *Return: Always (Success)
+ *main - prints the alphabet in lowercase, followed by a new line
+ *Return: Always 0
  */
 
 int main(void)
 
 {
+
 char ch;
 for (ch = 'a'; ch <= 'z'; ch++)
 {
-if (ch != 'q' && ch != 'q')
+if (ch != 'q' && ch != 'e')
 putchar(ch);
 }
-putchar('\n');
-
+putchar(10);
 return (0);
 }
-
